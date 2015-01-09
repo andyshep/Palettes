3ba52844122838bf67647a376c0be45afef6f76c
Andrew Shepard <shep.andy@gmail.com>
2015-01-09 08:00:16 -0800

    Use FRC with caching incremental store

--- Palettes/CachingIncrementalStore.swift
+++ Palettes/CachingIncrementalStore.swift
@@ -8,10 +8,14 @@
 
 import CoreData
 
-let kResourceIdentifierAttributeName = "__pal__resourceIdentifier"
-let kLastModifiedAttributeName = "__pal__lastModified"
+let kPALResourceIdentifierAttributeName = "__pal__resourceIdentifier"
+let kPALLastModifiedAttributeName = "__pal__lastModified"
+
+typealias InsertOrUpdateCompletion = (managedObjects:AnyObject, backingObjects:AnyObject) -> Void
 
 @objc(CachingIncrementalStore)
+
+/// An Incremental Store subclass for retrieving Palettes from the Colour Lovers API
 class CachingIncrementalStore : NSIncrementalStore {
     
     /// The cache of managed object ids
@@ -25,6 +29,10 @@ class CachingIncrementalStore : NSIncrementalStore {
     
     class var storeType: String {
         return NSStringFromClass(CachingIncrementalStore.self)
+    }
+    
+    override class func initialize() {
+        NSPersistentStoreCoordinator.registerStoreClass(self, forStoreType:self.storeType)
     }
     
     // MARK: - Lazy Accessors
@@ -73,12 +81,12 @@ class CachingIncrementalStore : NSIncrementalStore {
                 }
                 
                 let resourceIdProperty = NSAttributeDescription()
-                resourceIdProperty.name = kResourceIdentifierAttributeName
+                resourceIdProperty.name = kPALResourceIdentifierAttributeName
                 resourceIdProperty.attributeType = NSAttributeType.StringAttributeType
                 resourceIdProperty.indexed = true
                 
                 let lastModifiedProperty = NSAttributeDescription()
-                lastModifiedProperty.name = kLastModifiedAttributeName
+                lastModifiedProperty.name = kPALLastModifiedAttributeName
                 lastModifiedProperty.attributeType = NSAttributeType.DateAttributeType
                 lastModifiedProperty.indexed = false
                 
@@ -93,13 +101,12 @@ class CachingIncrementalStore : NSIncrementalStore {
         return augmentedModel
     }()
     
-    override class func initialize() {
-        NSPersistentStoreCoordinator.registerStoreClass(self, forStoreType:self.storeType)
-    }
+    // MARK: - NSIncrementalStore
     
     override func loadMetadata(error: NSErrorPointer) -> Bool {
         let uuid = NSProcessInfo.processInfo().globallyUniqueString
-        self.metadata = [NSStoreTypeKey : CachingIncrementalStore.storeType, NSStoreUUIDKey : uuid]
+        self.metadata = [NSStoreTypeKey : CachingIncrementalStore.storeType, NSStoreUUIDKey: uuid]
+        
         return true
     }
     
@@ -107,36 +114,34 @@ class CachingIncrementalStore : NSIncrementalStore {
         if request.requestType == .FetchRequestType {
             return self.executeFetchRequest(request, withContext: context, error: error)
         }
-        else if request.requestType == .SaveRequestType {
-            return self.executeSaveRequest(request, withContext: context, error: error)
-        }
         
         return nil
     }
     
     override func newValuesForObjectWithID(objectID: NSManagedObjectID, withContext context: NSManagedObjectContext, error: NSErrorPointer) -> NSIncrementalStoreNode? {
-        if let values = cache.objectForKey(objectID) as? NSDictionary {
-            return NSIncrementalStoreNode(objectID: objectID, withValues: values, version: 1)
+        let fetchRequest = NSFetchRequest(entityName: objectID.entity.name!)
+        fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
+        fetchRequest.fetchLimit = 1
+        fetchRequest.includesSubentities = false
+        
+        let refObj = self.referenceObjectForObjectID(objectID) as NSString
+        let predicate = NSPredicate(format: "%K = %@", kPALResourceIdentifierAttributeName, refObj.description)
+        fetchRequest.predicate = predicate
+        
+        var error: NSError? = nil
+        var results: [AnyObject]? = nil
+        let privateContext = self.backingManagedObjectContext
+        privateContext.performBlockAndWait(){
+            results = privateContext.executeFetchRequest(fetchRequest, error: &error)
         }
         
-        return nil
+        let attributeValues = (results?.count > 0) ? results?.last as NSDictionary : NSDictionary()
+        let node = NSIncrementalStoreNode(objectID: objectID, withValues: attributeValues, version: 1)
+        
+        return node
     }
     
     // MARK: - Private
-
-    func objectIdForNewObjectOfEntity(entityDescription:NSEntityDescription, cacheValues values:AnyObject!) -> NSManagedObjectID! {
-        if let dict = values as? NSDictionary {
-            let nativeKey = entityDescription.name
-            
-            if let referenceId = dict.objectForKey("id")?.stringValue {
-                let objectId = self.newObjectIDForEntity(entityDescription, referenceObject: referenceId)
-                cache.setObject(values, forKey: objectId)
-                return objectId
-            }
-        }
-        
-        return nil
-    }
     
     /**
     Executes a fetch request within the context provided
@@ -151,7 +156,7 @@ class CachingIncrementalStore : NSIncrementalStore {
     func executeFetchRequest(request: NSPersistentStoreRequest!, withContext context: NSManagedObjectContext!, error: NSErrorPointer) -> [AnyObject]! {
         var error: NSError? = nil
         let fetchRequest = request as NSFetchRequest
-        let backingContext = self.backingManagedObjectContext        
+        let backingContext = self.backingManagedObjectContext
         
         if fetchRequest.resultType == .ManagedObjectResultType {
             self.fetchRemoteObjectsWithRequest(fetchRequest, context: context)
@@ -177,60 +182,82 @@ class CachingIncrementalStore : NSIncrementalStore {
             
             return managedObjects
         }
+        else if fetchRequest.resultType == .ManagedObjectIDResultType {
+            let objectIds = backingContext.executeFetchRequest(fetchRequest, error: &error)
+            return []
+        }
+        else if fetchRequest.resultType == .CountResultType || fetchRequest.resultType == .DictionaryResultType {
+            return backingContext.executeFetchRequest(fetchRequest, error: &error)
+        }
         else {
             return nil
         }
     }
     
     /**
-    Executes a save request within the context provided
+    Insert or updates an entity set from the result provided
     
-    :param: request The request for the store.
-    :param: context The context to execure the request within
-    :param: error If an error occurs, on return contains an `NSError` object that describes the problem.
+    :param: result An set of `NSManagedObjects` that has been retrieved
+    :param: entity A valid entity within the model
+    :param: context A managed object context
+    :param: completion A function that accepts inserted objects and backing objects
     
-    :returns: An optional array of managed objects
+    :returns: success A Bool representing success or failure
     */
     
-    func executeSaveRequest(request: NSPersistentStoreRequest!, withContext context: NSManagedObjectContext!, error: NSErrorPointer) -> [AnyObject]! {
-        println("save requests not yet implemented")
-        
-        return []
-    }
-    
-    /**
-    Fetch remote objects
-    
-    :param: fetchRequest The request for the store.
-    :param: context The context to execure the request within
-    
-    :returns: An array of managed objects
-    */
-    
-    func fetchRemoteObjectsWithRequest(fetchRequest: NSFetchRequest, context: NSManagedObjectContext) -> Void {
-        let offset = fetchRequest.fetchOffset
-        let limit = fetchRequest.fetchLimit
-        let httpRequest = ColourLovers.TopPalettes.request(offset: offset, limit: limit)
-        
-        NetworkController.task(httpRequest, completion: { (data, error) -> Void in
-            var err: NSError?
-            let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &err) as [AnyObject]
-            let palettes = jsonResult.filter({ (obj: AnyObject) -> Bool in
-                return (obj is NSDictionary)
-            })
+    func insertOrUpdateObjects(result: [AnyObject]?, ofEntity entity: NSEntityDescription, context: NSManagedObjectContext, completion: InsertOrUpdateCompletion) -> Bool {
+        if let objects = result {
+            var managedObjects: [AnyObject] = []
+            var backingObjects: [AnyObject] = []
             
-            println("have \(palettes.count) palettes to parse")
+            for obj in objects {
+                if let paletteObj = obj as? NSDictionary {
+                    let name = paletteObj.stringValueForKey("title")
+                    let uniqueId = paletteObj.numberValueForKey("id").stringValue
+                    
+                    var error: NSError? = nil
+                    
+                    var backingObject: Palette? = nil
+                    
+                    let backingObjectId = self.objectIDFromBackingContextForEntity(entity, withResourceIdentifier: uniqueId)
+                    let backingContext = self.backingManagedObjectContext
+                    
+                    backingContext.performBlockAndWait(){
+                        if backingObjectId != nil {
+                            backingObject = backingContext.existingObjectWithID(backingObjectId!, error: &error) as? Palette
+                        }
+                        else {
+                            backingObject = NSEntityDescription.insertNewObjectForEntityForName(entity.name!, inManagedObjectContext: backingContext) as? Palette
+                            backingObject?.managedObjectContext?.obtainPermanentIDsForObjects([backingObject!], error: &error)
+                        }
+                    }
+                    
+                    backingObject?.setValue(uniqueId, forKey: kPALResourceIdentifierAttributeName)
+                    backingObject?.transform(dictionary: paletteObj)
+                    
+                    var managedObject: Palette? = nil
+                    context.performBlockAndWait({ () -> Void in
+                        if let objectId = self.objectIDForEntity(entity, withResourceIdentifier: uniqueId) {
+                            managedObject = context.existingObjectWithID(objectId, error: &error) as? Palette
+                        }
+                    })
+                    
+                    managedObject?.transform(dictionary: paletteObj)
+                    
+                    if backingObjectId != nil {
+                        context.insertObject(managedObject!)
+                    }
+                    
+                    managedObjects.append(managedObject!)
+                    backingObjects.append(backingObject!)
+                }
+            }
             
-            // TODO: handle result
-            
-        }).resume()
+            completion(managedObjects: managedObjects, backingObjects: backingObjects)
+            return true
+        }
         
-//        let entities = paletteObjs.map({ (item: NSDictionary) -> Palette in
-//            let objectId = self.objectIdForNewObjectOfEntity(fetchRequest.entity!, cacheValues: item)
-//            let palette = context.objectWithID(objectId) as Palette
-//            palette.transform(dictionary: item)
-//            return palette
-//        })
+        return false
     }
     
     /**
@@ -258,5 +285,100 @@ class CachingIncrementalStore : NSIncrementalStore {
         }
         
         return managedObjectId
+    }
+    
+    /**
+    Finds an objectID for an entity using an associated resource identifier. The objectId returned
+    will belong to the backing context
+    
+    :param: entity A valid entity within the model
+    :param: identifier A resource identifier
+    
+    :returns: objectId
+    */
+    
+    func objectIDFromBackingContextForEntity(entity:NSEntityDescription, withResourceIdentifier identifier:NSString?) -> NSManagedObjectID? {
+        if identifier == nil {
+            return nil
+        }
+        
+        let objectId = self.objectIDForEntity(entity, withResourceIdentifier: identifier)
+        var backingObjectId: NSManagedObjectID? = backingObjectIDCache.objectForKey(objectId!) as? NSManagedObjectID
+        if backingObjectId != nil {
+            return backingObjectId
+        }
+        
+        let fetchRequest = NSFetchRequest(entityName: entity.name!)
+        fetchRequest.resultType = NSFetchRequestResultType.ManagedObjectIDResultType
+        fetchRequest.fetchLimit = 1
+        
+        let predicate = NSPredicate(format: "%K = %@", kPALResourceIdentifierAttributeName, identifier!)
+        fetchRequest.predicate = predicate
+        
+        var error: NSError? = nil
+        let privateContext = self.backingManagedObjectContext
+        privateContext.performBlockAndWait() {
+            if let results = privateContext.executeFetchRequest(fetchRequest, error: &error) {
+                backingObjectId = results.last as? NSManagedObjectID
+            }
+        }
+        
+        if backingObjectId != nil {
+            backingObjectIDCache.setObject(backingObjectId!, forKey: objectId!)
+        }
+        
+        return backingObjectId
+    }
+    
+    func fetchRemoteObjectsWithRequest(fetchRequest: NSFetchRequest, context: NSManagedObjectContext) -> Void {
+        let offset = fetchRequest.fetchOffset
+        let limit = fetchRequest.fetchLimit
+        let httpRequest = ColourLovers.TopPalettes.request(offset: offset, limit: limit)
+        
+        NetworkController.task(httpRequest, completion: { (data, error) -> Void in
+            var err: NSError?
+            let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &err) as [AnyObject]
+            let palettes = jsonResult.filter({ (obj: AnyObject) -> Bool in
+                return (obj is NSDictionary)
+            })
+            
+            context.performBlockAndWait(){
+                let childContext = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
+                childContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+                childContext.parentContext = context
+                
+                childContext.performBlockAndWait(){
+                    let result = self.insertOrUpdateObjects(palettes, ofEntity: fetchRequest.entity!, context: childContext, completion:{(managedObjects: AnyObject, backingObjects: AnyObject) -> Void in
+                        
+                        childContext.saveOrLogError()
+                        
+                        self.backingManagedObjectContext.performBlockAndWait() {
+                            self.backingManagedObjectContext.saveOrLogError()
+                        }
+                        
+                        context.performBlockAndWait() {
+                            let objects = childContext.registeredObjects.allObjects
+                            objects.map({ (obj: AnyObject) -> Void in
+                                let childObject = obj as NSManagedObject
+                                let parentObject = context.objectWithID(childObject.objectID)
+                                context.refreshObject(parentObject, mergeChanges: true)
+                            })
+                        }
+                        
+                        println("incremental store finished refreshing \(palettes.count) from the network")
+                    })
+                }
+            }
+            
+        }).resume()
+    }
+}
+
+extension NSManagedObjectContext {
+    func saveOrLogError() -> Void {
+        var error: NSError? = nil
+        if !self.save(&error) {
+            println("error saving context: \(error)")
+        }
     }
 }
--- Palettes/PalettesContentStore.swift
+++ Palettes/PalettesContentStore.swift
@@ -9,14 +9,14 @@
 import UIKit
 import CoreData
 
-class PalettesContentStore: NSObject, UICollectionViewDataSource {
+class PalettesContentStore: NSObject, UICollectionViewDataSource, NSFetchedResultsControllerDelegate {
     private var objects: [Palette] = []
     
     weak var collectionView: UICollectionView? {
         didSet {
             self.objects = []
             self.collectionView?.reloadData()
-            self.executeFetchRequest(offset: 0)
+            self.performFetch()
         }
     }
     
@@ -27,7 +27,7 @@ class PalettesContentStore: NSObject, UICollectionViewDataSource {
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.objects.count
+        return self.fetchedResultsController.fetchedObjects?.count ?? 0
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -44,28 +44,58 @@ class PalettesContentStore: NSObject, UICollectionViewDataSource {
         return cell
     }
     
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.collectionView?.reloadData()
+    }
+    
+    var fetchedResultsController: NSFetchedResultsController {
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let context = CoreDataManager.sharedManager.managedObjectContext!
+        
+        let fetchRequest = self.palettesFetchRequest(offset: 0)
+        
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: "Palettes")
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+        return _fetchedResultsController!
+    }
+    var _fetchedResultsController: NSFetchedResultsController? = nil
+    
     // MARK: - Private
     
-    func executeFetchRequest(#offset: Int) -> Void {
-        let request = palettesFetchRequest(offset: offset)
-        let asyncRequest = NSAsynchronousFetchRequest(fetchRequest: request) { (result) -> Void in
-            let count = result.finalResult?.count
-            if count > 0 {
-                println("executeFetchRequest received \(count!) objects")
-                
-                if let palettes = result.finalResult as? [Palette] {
-                    self.objects += palettes
-                    self.collectionView?.reloadData()
-                }
-            }
-            else {
-                println("received no objects...")
-            }
+//    func executeFetchRequest(#offset: Int) -> Void {
+//        let request = palettesFetchRequest(offset: offset)
+//        let asyncRequest = NSAsynchronousFetchRequest(fetchRequest: request) { (result) -> Void in
+//            let count = result.finalResult?.count
+//            if count > 0 {
+//                println("executeFetchRequest received \(count!) objects")
+//                
+//                if let palettes = result.finalResult as? [Palette] {
+//                    self.objects += palettes
+//                    self.collectionView?.reloadData()
+//                }
+//            }
+//            else {
+//                println("received no objects...")
+//            }
+//        }
+//
+//        var error: NSError?
+//        let context = CoreDataManager.sharedManager.managedObjectContext
+//        let result = context?.executeRequest(asyncRequest, error: &error)
+//    }
+    
+    func performFetch() -> Void {
+        var error: NSError? = nil
+        if !self.fetchedResultsController.performFetch(&error) {
+            println("Error performing fetch: \(error)")
         }
-
-        var error: NSError?
-        let context = CoreDataManager.sharedManager.managedObjectContext
-        let result = context?.executeRequest(asyncRequest, error: &error)
     }
     
     func palettesFetchRequest(#offset:Int) -> NSFetchRequest {
@@ -78,7 +108,8 @@ class PalettesContentStore: NSObject, UICollectionViewDataSource {
     }
     
     func objectAtIndexPath(indexPath:NSIndexPath) -> Palette {
-        let object = self.objects[indexPath.row]
+//        let object = self.objects[indexPath.row]
+        let object = self.fetchedResultsController.fetchedObjects?[indexPath.row] as Palette
         return object
     }
 }
